ebab94f66a9d429dae46bd8d3bdb4ab858075e13
drod75 <dr507498@gmail.com>
2025-06-20 13:08:32 -0400

    Editing app to be a basic streamlit app

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from weeaboo-buddy!")
+import streamlit as st
 
+def app():
+    st.title('Development App')
+    st.markdown('This is a basic app for development purposes at the moment, actual content wil come soon...')
 
 if __name__ == "__main__":
-    main()
+    app()
